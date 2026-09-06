a448cf6057bbfd39f997141936a4d35e3a5bd095
osh108606 <osh108606@naver.com>
2026-09-06 11:54:12 +0900

    산탄범위 시각화(에디터한정)

--- Assets/_DungeonHunt/Scripts/Weapons/FireMode/ConeFireMode.cs
+++ Assets/_DungeonHunt/Scripts/Weapons/FireMode/ConeFireMode.cs
@@ -12,16 +12,25 @@ public class ConeFireMode : IWeaponFireMode
 
         int enemyMask = LayerMask.GetMask(EnemyLayerName);
         int wallMask = LayerMask.GetMask(WallLayerName);
+        
+        Vector2 leftBoundary = Quaternion.Euler(0, 0, halfAngle) * direction;
+        Vector2 rightBoundary = Quaternion.Euler(0, 0, -halfAngle) * direction;
+        Debug.DrawRay(origin, leftBoundary * range, Color.yellow, 1f);
+        Debug.DrawRay(origin, rightBoundary * range, Color.yellow, 1f);
+        Debug.DrawRay(origin, direction * range, Color.red, 1f); 
 
         Collider2D[] hits = Physics2D.OverlapCircleAll(origin, range, enemyMask);
 
         foreach (var hit in hits)
         {
             Vector2 toTarget = (Vector2)hit.transform.position - origin;
-            
+
             if (Vector2.Angle(direction, toTarget) > halfAngle) continue;
-            
+
             RaycastHit2D wallCheck = Physics2D.Raycast(origin, toTarget.normalized, toTarget.magnitude, wallMask);
+
+            Debug.DrawLine(origin, hit.transform.position, wallCheck.collider != null ? Color.gray : Color.green, 1f);
+
             if (wallCheck.collider != null) continue;
 
             if (hit.TryGetComponent<IDamageable>(out var damageable))
